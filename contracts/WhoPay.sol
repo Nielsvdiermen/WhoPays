7375139c6a08d7a35e358f7c0f354e994d8eae04
Niels <nielsvdiermen@gmail.com>
2017-06-08 01:06:08 +0200

    new contract

--- contracts/WhoPay.sol
+++ contracts/WhoPay.sol
@@ -1,38 +1,35 @@
-pragma solidity ^0.4.6;
+pragma solidity ^0.4.11;
 // We have to specify what version of compiler this code will compile with
 
-contract Whopay {
-  //struct db for contract resolving
-  //struct memberOw{
-  //  uint amount;
- // }
-  
+contract Whopay {  
   // Struct DB for each member
   struct member {
     uint memberPaid;
     uint balance;
     bool positive;
-    mapping (address => uint) memberOwing;
+    uint totalOws;
   }
 
   //group parameters
   string public groupName;
-  string public groupDescription;
   uint public totalAmount;
+  uint public totalOwed;
+  bool public contractClosed;
 
   mapping (address => member) public memberInfo;
 
   address[] public memberList;
 
   //contructor function
-  function Whopay(string name, string description, address[] memberAddresses){
+  function Whopay(string name, address[] memberAddresses){
     groupName = name;
-    groupDescription = description;
     memberList = memberAddresses;
+    contractClosed = false;
   }
 
   //adding
   function memberPay(address member, uint paid) returns(uint){
+    if (contractClosed) throw;
     if(paid > 0 && validMember(member)){
       memberInfo[member].memberPaid += paid;
       totalAmount += paid;
@@ -42,6 +39,7 @@ contract Whopay {
  
   //removing
   function memberReduce(address member, uint paid) returns(uint){
+    if (contractClosed) throw;
     if(paid > 0 && validMember(member)){
       if((memberInfo[member].memberPaid - paid) < 0 || (totalAmount - paid) < 0) throw;
       memberInfo[member].memberPaid -= paid;
@@ -52,6 +50,7 @@ contract Whopay {
 
   //checking if address is valid
   function validMember(address member) constant returns (bool) {
+    if (contractClosed) throw;
      for(uint i = 0; i < memberList.length; i++) {
         if (memberList[i] == member) {
           return true;
@@ -62,11 +61,12 @@ contract Whopay {
 
   //status for 1 user
   function memberStatus(address member) constant returns(uint){
+    if (contractClosed) throw;
     if (validMember(member)) return memberInfo[member].memberPaid;
   }
 
-  function groupInfo() constant returns(string,string){
-    return (groupName,groupDescription);
+  function groupInfo() constant returns(string){
+    return (groupName);
   }
 
   //status for all users
@@ -79,6 +79,7 @@ contract Whopay {
   }
 
   function contractResolve() returns(bool){
+    if (contractClosed) throw;
     //total divided by total user > get balance per user
     uint perUser = totalAmount / memberList.length;
 
@@ -94,49 +95,49 @@ contract Whopay {
       else{
         userBalance = perUser - paid;
         memberInfo[memberList[i]].positive = false;
+        memberInfo[memberList[i]].totalOws = userBalance;
+        totalOwed += userBalance;
       }
       //set balance for each member
       memberInfo[memberList[i]].balance = userBalance;
     }
-    //check balance against other users -> for loop
-    //to pay address + topay
     return true;
   }
 
-  function getOwed(){
-    for(uint i = 0; i < memberList.length; i++) {
-    //loop over members
-    //if member = negative
-      if(memberInfo[memberList[i]].positive == false){
-        //loop over other members
-        for(uint j = 0; j < memberList.length; j++) {
-          //only against other positive members
-          if(memberInfo[memberList[j]].positive == true && memberInfo[memberList[j]].balance > 0 && memberInfo[memberList[i]].balance > 0){
-            //add payments 
-            //reduce positive on other member
-            if (memberInfo[memberList[j]].balance > memberInfo[memberList[i]].balance){
-              memberInfo[memberList[j]].balance -= memberInfo[memberList[i]].balance;
-              memberInfo[memberList[i]].memberOwing[memberList[j]] = memberInfo[memberList[i]].balance;
-              memberInfo[memberList[i]].balance = 0;
-            }
-            else{
-              memberInfo[memberList[i]].balance -= memberInfo[memberList[j]].balance;
-              memberInfo[memberList[i]].memberOwing[memberList[j]] = memberInfo[memberList[j]].balance;
-              memberInfo[memberList[j]].balance = 0;
-            }
-          }
+  function memberAddPay(address member) payable{
+    if (contractClosed) throw;
+    uint memberpays = msg.value;
+    if(memberpays > memberInfo[member].totalOws) throw;
+    memberInfo[member].totalOws -= memberpays;
+    totalOwed -= memberpays;
+    
+    if (totalOwed == 0){
+    //pay
+      for(uint i = 0; i < memberList.length; i++) {
+        if(memberInfo[memberList[i]].positive == true){
+          payOut(memberList[i]);
         }
       }
+    //close contract
+    contractClosed = true;
     }
   }
 
-  //test functions
-  function userBalance(address member) constant returns(uint, bool){
-    return (memberInfo[member].balance,memberInfo[member].positive);
+  function payOut(address member) private{
+    if (memberInfo[member].balance > 0){
+        member.transfer(memberInfo[member].balance);
+    }
   }
 
-  function userOws(address member,address other) constant returns(uint){
-    return memberInfo[member].memberOwing[other];
+  function userBalance(address member) constant returns(uint, bool){
+    if (contractClosed) throw;
+    uint amount;
+    if (memberInfo[member].positive == true){
+      amount = memberInfo[member].balance;
+    }else{
+      amount = memberInfo[member].totalOws;
+    }
+    return (amount,memberInfo[member].positive);
   }
 }
 
